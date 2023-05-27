e73aba6b737b2e02f671140cd4478e5d6ce6af10
Jericho <jerichobantiquete@gmail.com>
2023-05-27 14:42:38 +0800

    feat: redirect to specific dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         if (Auth::user()->hasRole('user')) {
-            return view('user.dashboard.index');
+            return Inertia::render('User/Dashboard/Dashboard');
         } elseif (Auth::user()->hasRole('administrator')) {
-            return view('administrator.dashboard.index');
+            return Inertia::render('Administrator/Dashboard/Dashboard');
         };
     }
 }
--- resources/views/administrator/dashboard/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-sidebar-layout>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900">
-                {{ __("You're logged in administrator!") }}
-            </div>
-        </div>
-    </div>
-</x-app-sidebar-layout>
