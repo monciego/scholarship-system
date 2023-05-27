23ca2a473c6aa9905ea9e698ee702233edd63fa4
Jericho <jerichobantiquete@gmail.com>
2023-05-27 12:40:17 +0800

    fix: fix conflict :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,19 @@
-<<<<<<< HEAD
 import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
-=======
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
->>>>>>> inertia-version
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-<<<<<<< HEAD
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
-=======
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
->>>>>>> inertia-version
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-<<<<<<< HEAD
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-                inter: ["Inter", ...defaultTheme.fontFamily.sans],
-                montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
-=======
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
->>>>>>> inertia-version
             },
         },
     },
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-<<<<<<< HEAD
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-=======
-            input: 'resources/js/app.js',
->>>>>>> inertia-version
+            input: "resources/js/app.js",
             refresh: true,
         }),
         vue({
