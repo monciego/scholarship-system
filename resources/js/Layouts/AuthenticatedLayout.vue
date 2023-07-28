662fdc6016d026da626995ebae19267c6e25a512
Jericho <jerichobantiquete@gmail.com>
2023-07-28 18:44:07 +0800

    refactor: add navigation for mobile

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -165,6 +165,27 @@ const showingNavigationDropdown = ref(false);
                             </svg>
                             Dashboard
                         </ResponsiveNavLink>
+                        <ResponsiveNavLink
+                            v-if="$page.props.role.admin"
+                            :href="route('register.representative')"
+                            :active="route().current('register.representative')"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="text-gray-300 mr-3 flex-shrink-0 h-5 w-5"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                                />
+                            </svg>
+                            Register Representative
+                        </ResponsiveNavLink>
                     </nav>
                 </div>
             </div>
