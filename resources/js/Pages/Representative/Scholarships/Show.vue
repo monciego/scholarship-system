8b8289deb3e8d24a80e099cc1a303c39f0102c0e
Jericho <jerichobantiquete@gmail.com>
2023-11-12 22:07:07 +0800

    feat: show requirements in scholarship details

--- resources/js/Pages/Representative/Scholarships/Show.vue
+++ resources/js/Pages/Representative/Scholarships/Show.vue
@@ -172,6 +172,58 @@ defineProps(["scholarship"]);
                             {{ scholarship.details }}
                         </dd>
                     </div>
+                    <div
+                        v-if="
+                            scholarship.requirements !== null &&
+                            scholarship.requirements !== ''
+                        "
+                        class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0"
+                    >
+                        <dt class="text-sm font-medium leading-6 text-gray-900">
+                            Requirements
+                        </dt>
+                        <dd
+                            class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0"
+                        >
+                            <ul
+                                role="list"
+                                class="divide-y divide-gray-100 rounded-md border border-gray-200"
+                            >
+                                <li
+                                    v-for="requirement in scholarship.requirements.split(
+                                        '|'
+                                    )"
+                                    class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6"
+                                >
+                                    <div class="flex w-0 flex-1 items-center">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="h-5 w-5 flex-shrink-0 text-gray-400"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+                                            />
+                                        </svg>
+
+                                        <div
+                                            class="ml-4 flex min-w-0 flex-1 gap-2"
+                                        >
+                                            <span
+                                                class="truncate font-medium"
+                                                >{{ requirement }}</span
+                                            >
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </dd>
+                    </div>
                 </dl>
             </div>
         </div>
