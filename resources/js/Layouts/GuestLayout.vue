29ce27be0839a9434558396dd366db401b5a5a18
Jericho <jerichobantiquete@gmail.com>
2023-12-03 03:04:13 +0800

    style: improve registration ui

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -8,7 +8,7 @@ import Footer from "@/Shared/Auth/Footer.vue";
         class="min-h-screen flex flex-col sm:justify-center items-center bg-gray-100"
     >
         <Header />
-        <div class="sm:mx-auto px-6 sm:px-0 mt-28 sm:mt-10 w-full sm:max-w-md">
+        <div class="sm:mx-auto px-6 sm:px-0 mt-28 sm:mt-10 w-full sm:max-w-3xl">
             <div
                 class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 h-[22rem] overflow-y-scroll"
             >
