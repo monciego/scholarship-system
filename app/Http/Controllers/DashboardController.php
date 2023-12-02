07c90dc33579d22ae6aa74eae23d1de6c89546ea
Jericho <jerichobantiquete@gmail.com>
2023-12-02 13:59:44 +0800

    feat: machine learning knn for recommending scholarships (wip)

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\ApplicationForm;
 use App\Models\Scholarship;
 use App\Models\User;
+use League\Csv\Reader;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use Phpml\Classification\KNearestNeighbors;
+use Phpml\Dataset\ArrayDataset;
+use Phpml\FeatureExtraction\TfIdfTransformer;
+use Phpml\FeatureExtraction\TfIdfVectorizer;
+use Phpml\Metric\Accuracy;
 
 class DashboardController extends Controller
 {
@@ -18,6 +24,40 @@ class DashboardController extends Controller
         if ($user->hasRole('user')) {
             $userData = User::where('id', auth()->user()->id)->firstOrFail();
             $existing_scholarships = json_decode($userData->have_existing_scholarship, true);
+
+            // Get the absolute path to the CSV file
+            $csvFilePath = __DIR__ . '/dataset.csv';
+            $csv = Reader::createFromPath($csvFilePath, 'r');
+            $data = $csv->getRecords();
+            $csv->setHeaderOffset(0);
+            $headers = $csv->getHeader(); // Get header names if needed
+
+            $data = iterator_to_array($data);
+
+
+            $classification = new KNearestNeighbors($k = 3);
+
+            $samples = [];
+            $labels = [];
+
+            foreach ($data as $row) {
+                // Ensure that the following line corresponds to the structure of your CSV
+                $samples[] = array_slice($row, 0, -1);
+                $labels[] = end($row);
+            }
+
+            $samples = array_map(fn($row) => array_slice($row, 0, -1), $data);
+            $labels = array_column($data, count($data[0]) - 1);
+
+            $classifier = new KNearestNeighbors();
+            $classifier->train($samples, $labels);
+
+
+            $house_hold_per_capita_income = User::where('id', auth()->user()->id)->value('house_hold_per_capita_income');
+            $number_of_studying_siblings = User::where('id', auth()->user()->id)->value('number_of_studying_siblings');
+
+            $prediction = $classifier->predict([$house_hold_per_capita_income, $number_of_studying_siblings]);
+
             return Inertia::render('User/Dashboard/Dashboard', compact('userData', 'existing_scholarships'));
         } elseif ($user->hasRole('administrator')) {
             $scholarshipCount = Scholarship::count();
