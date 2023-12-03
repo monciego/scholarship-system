82fc3b3d098d304c28552df47e41927e11a197d8
Jericho <jerichobantiquete@gmail.com>
2023-12-03 12:42:02 +0800

    style: scholarship card mobile style

--- resources/js/Shared/ScholarshipCard.vue
+++ resources/js/Shared/ScholarshipCard.vue
@@ -23,7 +23,7 @@ dayjs.extend(LocalizedFormat);
         <div class="flex-1 relative pt-16 px-6 pb-4 md:px-8">
             <div
                 v-if="scholarship.status === 'onHold'"
-                class="bg-yellow-500 inline py-2 px-4 text-white rounded absolute top-4 right-4"
+                class="bg-yellow-500 sm:text-base text-sm inline py-2 px-4 text-white rounded absolute top-4 right-4"
             >
                 ON HOLD
             </div>
@@ -52,17 +52,17 @@ dayjs.extend(LocalizedFormat);
                     />
                 </svg>
             </div>
-            <h3 class="text-xl font-medium text-gray-900">
+            <h3 class="text-lg sm:text-xl font-medium text-gray-900">
                 {{ scholarship.scholarshipName }}
             </h3>
             <p
                 :class="
                     scholarship.scholarshipType === 'government scholarship'
-                        ? 'bg-yellow-600'
-                        : 'bg-indigo-600 ' &&
+                        ? 'bg-yellow-600 sm:text-base text-sm'
+                        : 'bg-indigo-600  sm:text-base text-sm' &&
                           scholarship.scholarshipType === 'private scholarship'
-                        ? 'bg-slate-700'
-                        : 'bg-indigo-600 '
+                        ? 'bg-slate-700 sm:text-base text-sm'
+                        : 'bg-indigo-600  sm:text-base text-sm'
                 "
                 class="text-sm my-2 capitalize inline-block text-white p-1 px-4 rounded"
             >
@@ -79,7 +79,7 @@ dayjs.extend(LocalizedFormat);
             </p>
             <a
                 href="#"
-                class="text-base font-medium text-indigo-700 hover:text-indigo-600"
+                class="text-sm sm:text-base font-medium text-indigo-700 hover:text-indigo-600"
             >
                 More Details <span aria-hidden="true"> &rarr;</span>
             </a>
@@ -93,8 +93,10 @@ dayjs.extend(LocalizedFormat);
                     Slots: {{ scholarship.application_form.length }} /
                     {{ scholarship.slot }}
                 </p>
-                <p>Available For: {{ scholarship.availableFor }}</p>
-                <p>
+                <p class="sm:text-base text-sm">
+                    Available For: {{ scholarship.availableFor }}
+                </p>
+                <p class="sm:text-base text-sm">
                     Deadline: {{ dayjs(scholarship.deadlineAt).format("LL") }}
                 </p>
             </div>
@@ -106,14 +108,14 @@ dayjs.extend(LocalizedFormat);
                     scholarship.scholarshipType === 'government scholarship'
                 "
                 type="button"
-                class="inline-flex items-center px-6 py-3 border border-transparent text-base leading-4 font-medium rounded-md cursor-not-allowed shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                class="inline-flex items-center px-6 py-2 sm:py-3 border border-transparent text-sm sm:text-base leading-4 font-medium rounded-md cursor-not-allowed shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
             >
                 Full
             </button>
             <button
                 v-else-if="scholarship.status === 'onHold'"
                 type="button"
-                class="inline-flex items-center px-6 py-3 border border-transparent text-base leading-4 font-medium rounded-md cursor-not-allowed shadow-sm text-white bg-slate-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
+                class="inline-flex items-center px-6 py-2 sm:py-3 border border-transparent text-sm sm:text-base leading-4 font-medium rounded-md cursor-not-allowed shadow-sm text-white bg-slate-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
             >
                 On Hold
             </button>
@@ -127,7 +129,7 @@ dayjs.extend(LocalizedFormat);
                     scholarship.scholarshipType !== 'private scholarship' &&
                     scholarship.scholarshipType !== 'academic scholarship'
                 "
-                class="inline-flex items-center px-6 py-3 border border-transparent text-base leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                class="inline-flex items-center px-6 py-2 sm:py-3 border border-transparent text-sm sm:text-base leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
                 Apply Scholarship
             </Link>
@@ -141,7 +143,7 @@ dayjs.extend(LocalizedFormat);
                 v-else-if="
                     scholarship.scholarshipType === 'academic scholarship'
                 "
-                class="inline-flex items-center px-6 py-3 border border-transparent text-base leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                class="inline-flex items-center px-6 py-2 sm:py-3 border border-transparent text-sm sm:text-base leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
                 Pass Requirements
             </Link>
